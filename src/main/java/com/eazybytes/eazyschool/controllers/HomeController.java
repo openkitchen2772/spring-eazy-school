abc34f204d2628f047f34e66b4c1f0f4d15b0ec2
openkitchen2772 <53619884+openkitchen2772@users.noreply.github.com>
2022-09-05 18:41:12 +0800

    feat: add homepage

--- src/main/java/com/eazybytes/eazyschool/controllers/HomeController.java
+++ src/main/java/com/eazybytes/eazyschool/controllers/HomeController.java
@@ -1,13 +1,15 @@
 package com.eazybytes.eazyschool.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String Home() {
+    @RequestMapping(value = { "", "/", "home" })
+    public String Home(Model model) {
+        model.addAttribute("username", "John Wick 2");
         return "home.html";
     }
 
